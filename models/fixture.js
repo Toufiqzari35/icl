79d5fe03e55880dc9422806013c659d131b2d162
pulkit171112236 <pulkit171112236@gmail.com>
2023-02-02 16:31:37 +0530

    fix: fixture - delete old data, seperate teamName teamData fields

--- models/fixture.js
+++ models/fixture.js
@@ -2,8 +2,12 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const FixtureSchema = Schema({
-  round: {
+  index: {
     type: Number,
+    required: true,
+  },
+  round: {
+    type: String,
   },
   match: {
     type: Number,
@@ -11,21 +15,26 @@ const FixtureSchema = Schema({
   ground: {
     type: String,
   },
-  teamA: {
-    type: Schema.Types.ObjectId,
-    ref: 'Team',
-  },
-  teamB: {
-    type: Schema.Types.ObjectId,
-    ref: 'Team',
-  },
   date: {
     type: String,
   },
   time: {
     type: String,
   },
+  teamA: {
+    type: String,
+  },
+  teamB: {
+    type: String,
+  },
   result: {
+    type: String,
+  },
+  teamAData: {
+    type: Schema.Types.ObjectId,
+    ref: 'Team',
+  },
+  teamBData: {
     type: Schema.Types.ObjectId,
     ref: 'Team',
   },
