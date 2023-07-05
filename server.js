cd3781ea84f2121393bd4385f467a92cb99273e2
Anshuman Kundu <kundu4coding@gmail.com>
2023-07-05 11:40:25 +0530

    Build uploaded

--- server.js
+++ server.js
@@ -137,7 +137,7 @@ app.use((err, req, res, next) => {
   })
 })
 
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 8000;
 // connect mongoose client then listen to PORT
 mongoose.set('strictQuery', true)
 mongoose
